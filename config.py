6de0a9e7af0196ed420359468ff635e90a490a4a
Gabriele Salvatori <salvatorigabriele@gmail.com>
2015-05-15 14:06:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ### SCRIPT MAIN CONFIGURATION | EDIT HERE ###
 
-slack_team_token = "xoxp-4490583233-4469009390-4898567836-bc20d7" 	# token available here: https://api.slack.com/web at the bottom of the page
+slack_team_token = "" 	# token available here: https://api.slack.com/web at the bottom of the page
 
 log_file = "/var/log/slackify.log"
 channels = "dev"													# channels you want to watch, separated by commas (at the moment only one channel works)
