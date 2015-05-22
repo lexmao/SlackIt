9f1b6687606115691282541fcd0f5c7b81e688e0
Gabriele Salvatori <salvatorigabriele@gmail.com>
2015-05-22 09:56:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 slack_team_token = "" 	# token available here: https://api.slack.com/web at the bottom of the page
 
-log_file = "/var/log/slackify.log"
+log_file = "/var/log/slackit.log"
 channels = "dev"													# channels you want to watch, separated by commas (at the moment only one channel works)
 notification_time_appear = 10										# notification display time (in seconds)
 check_notification_time = 20										# time interval for notification checks (in seconds)
