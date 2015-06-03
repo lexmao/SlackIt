d3ccf6ca422b3cabdec464be3f34f56aaba6082c
Gabriele <gabriele.s@21gear.com>
2015-06-03 10:50:08 +0200

    Improved structure of files

--- notify.py
+++ notify.py
@@ -31,28 +31,6 @@ from registry import *
 class Notify():
 
 	
-	#def set_notify_blacklist(self):
-
-
-	# def set_notify_author(self,author):
-		
-	# 	registry.update({"author":author})
-
-
-	# def set_notify_channel(self,channel):
-		
-	# 	registry.update({"channel":channel})
-
-
-	# def set_notify_title(self,title):
-	# 	title_with_author = "%s '%s' channel (from: %s)" % (title,registry['channel'],registry['author'])
-	# 	registry.update({"title":title_with_author})
-
-
-	# def set_notify_description(self,description):
-	# 	registry.update({"description":description})
-
-
 	def run_instance(self):
 
 		def_title = current_notify["title"]
