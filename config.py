a73d2c0eedac6ea8a1a7b9c10da76996f51de401
Gabriele Salvatori <salvatorigabriele@gmail.com>
2015-06-04 22:42:05 +0200

    Removed old token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ### SCRIPT MAIN CONFIGURATION | EDIT HERE ###
 
-slack_team_token = "xoxp-4490583233-4469009390-5205667234-31e702" 	                 # token available here: https://api.slack.com/web at the bottom of the page
+slack_team_token = "" 	                         # token available here: https://api.slack.com/web at the bottom of the page
 log_file = "/var/log/slackit.log"		 # channels you want to watch, separated by commas (at the moment only one channel works)
 notification_time_appear = 10			 # notification display time (in seconds)
 # check_notification_time = 20			 # time interval for notification checks (in seconds)
